ee8b8367d46fa03d8cfa130e00dda3bec1d881e3
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-23 20:46:23 +0530

    Implement backend server

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next) => {
-  console.log("First middleware");
-  next();
-})
 
-app.use((req,res,next) => {
-  res.send("Hello");
+app.use('/api/posts',(req,res,next) => {
+  const posts = [
+    {id:'post-1',title:'first server side post',content:'first server side post content'},
+    {id:'post-2',title:'second server side post',content:'second server side post content'}
+  ]
+  res.status(200).json({
+    message:'posts fetched',
+    posts
+  });
 })
 
 module.exports = app;

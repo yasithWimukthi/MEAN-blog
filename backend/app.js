3c55d5e0907b0138e2a307eb850342372581015a
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-24 16:58:38 +0530

    Fetch data from database

--- backend/app.js
+++ backend/app.js
@@ -43,15 +43,18 @@ app.post('/api/posts',(req,res,next) => {
 
 
 app.get('/api/posts',(req,res,next) => {
-  const posts = [
-    {id:'post-1',title:'first server side post',content:'first server side post content'},
-    {id:'post-2',title:'second server side post',content:'second server side post content'}
-  ]
-  res.status(200).json({
-    message:'posts fetched',
-    posts
-  });
-
+  // const posts = [
+  //   {id:'post-1',title:'first server side post',content:'first server side post content'},
+  //   {id:'post-2',title:'second server side post',content:'second server side post content'}
+  // ]
+  Post.find()
+    .then(documents => {
+      console.log(documents)
+      res.status(200).json({
+        message:'posts fetched',
+        posts: documents
+      });
+    })
 })
 
 module.exports = app;

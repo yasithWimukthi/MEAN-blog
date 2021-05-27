ea52208be1b171c719a7f1a6970ef687641564b6
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-27 21:01:22 +0530

    upload images

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   private mode = 'create';
   private postId:string;
+  imagePreview:string | ArrayBuffer;
   post:Post;
   isLoading = false;
   form:FormGroup;
@@ -28,7 +29,8 @@ export class PostCreateComponent implements OnInit {
       title: new FormControl(null,{
         validators:[Validators.required,Validators.minLength(3)]
       }),
-      content: new FormControl(null,{validators:[Validators.required]})
+      content: new FormControl(null,{validators:[Validators.required]}),
+      image: new FormControl(null,{validators:[Validators.required]})
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -65,5 +67,16 @@ export class PostCreateComponent implements OnInit {
       this.postService.updatePost(this.postId,this.form.value.title,this.form.value.content);
     }
     this.form.reset();
+  }
+
+  onImagePick(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file})
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(file);
   }
 }

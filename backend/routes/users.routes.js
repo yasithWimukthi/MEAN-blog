7bb7ed28990a804f2bb39a618c74da865117cf0c
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-03 17:28:38 +0530

    create user signup route

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
+const User = require('../models/user');
+
+router.post('/signup',(req, res, next) =>{
+  bcrypt.hash(req.body.password,10)
+    .then(hash =>{
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+
+      user.save()
+        .then((result) =>{
+          res.status(201).json({
+            message: 'User created',
+            result
+          })
+        })
+        .catch((error) =>{
+          res.status(500).json({error})
+        })
+    });
+
+})
 
 
 module.exports = router;

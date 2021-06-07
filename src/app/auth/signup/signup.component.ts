8850340e1ac093f2dd25d67fee13032b233a0f2c
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-07 20:45:17 +0530

    handle authentication errors

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthService} from "../auth-service.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit,OnDestroy {
   isLoading = false;
+  private _authSubscription: Subscription;
 
   constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
+    this._authSubscription = this.auth.getAuthStateListener().subscribe(
+      authState => this.isLoading = false
+    );
   }
 
   onSignup(signupForm: NgForm) {
     if (signupForm.invalid) return;
     this.isLoading = true;
     this.auth.createUser(signupForm.value.email,signupForm.value.password);
+  }
+
+  ngOnDestroy(): void {
+    this._authSubscription.unsubscribe();
   }
 }

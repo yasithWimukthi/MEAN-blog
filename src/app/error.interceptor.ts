81f982c4d2ff4efde6da52692e25c0affc6aa0b9
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-12 21:40:20 +0530

    Implement error component

--- src/app/error.interceptor.ts
+++ src/app/error.interceptor.ts
@@ -15,7 +15,13 @@ export class ErrorInterceptor implements HttpInterceptor{
     // @ts-ignore
     return next.handle(req).pipe(
       catchError((error:HttpErrorResponse) =>{
-        this.dialog.open(ErrorComponent);
+        let errorMessage = "An unknown error occurred!";
+
+        if(error.error.message){
+          errorMessage = error.error.message;
+        }
+
+        this.dialog.open(ErrorComponent, {data: {message:errorMessage}});
         return throwError(error);
       })
     );
